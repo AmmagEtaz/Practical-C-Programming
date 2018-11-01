40ff24c54909b1eb62b3c05aee92415b7dc5d04a
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-11-01 18:05:22 +0100

    Update and rename ex 4-4.c to 04.c

--- chapter 04/04.c
+++ chapter 04/04.c
@@ -1,3 +1,9 @@
+/********************************************************
+Write a program to print "HELLO" in big block letters; 
+each letter should have a height of seven characters 
+and a width of five characters.
+********************************************************/
+
 #include <stdio.h>
 
 int main()
